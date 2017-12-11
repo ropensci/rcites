391def6029d0c63cbfbf9e569f51102a83e3620e
Yasmine-Verzelen <yasmine.verzelen@inbo.be>
2017-12-11 16:41:50 +0100

    taxon_map

---
+++ R/taxon_map.R
@@ -0,0 +1,8 @@
+
+
+taxon_map <- function(cnx, taxon){
+  map1 <- taxon_distribution(cnx, query_taxon = taxon, country_only = TRUE)
+map2 <- joinCountryData2Map(map1, joinCode="ISO2", nameJoinColumn = "iso2", nameCountryColumn = "distribution")
+mapCountryData(mapToPlot = map2, nameColumnToPlot = "taxon", mapTitle = taxon, addLegend = FALSE)
+}
+taxon_map(cnx, "Branta canadensis")
