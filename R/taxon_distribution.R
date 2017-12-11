0fccf5fe9098a2edc43238538ccdf90fe14baa0b
Nacho <nacho@air>
2017-12-11 15:42:39 +0100

    Distribution working!

--- R/taxon_distribution.R
+++ R/taxon_distribution.R
@@ -1,11 +1,11 @@
-#' Access data...
+#' Access distribution data
 #'
 #' Query CITES.
 #'
 #' @param cnx the output of your cites_connect() function.
 #' @param query a query... character
-#' @param appendix_only a logical
-#' @return xml so far...
+#' @param country_only a logical
+#' @return data frame or list...
 #'
 #' @importFrom RCurl getURI
 #' @importFrom XML xmlToList
@@ -13,26 +13,24 @@
 #' @export
 #' @examples
 #' cnx <- sppplus_connect(token = 'ErJcYxUsIApHLCLOxiJ1Zwtt')
-#' sppplus_taxonconcept(cnx, query = 'Smaug giganteus', appendix_only = TRUE)
-#' sppplus_taxonconcept(cnx, query = 'Homo sapiens', appendix_only = TRUE)
-
-taxon_distribution <- function(cnx, query_taxon = "Smaug giganteus", 
-                                     country = "Spain") {
+#' taxon_distribution(cnx, query_taxon = 'Loxodonta africana', country_only = TRUE)
+taxon_distribution <- function(cnx, query_taxon = "Loxodonta africana", 
+                                     country_only = TRUE) {
   tax <- sppplus_taxonconcept(cnx, query = query_taxon, appendix_only = TRUE)
   temp <- getURI(url = paste(cnx[[1]], 
-                             "taxon_concepts/:", tax$id, 
+                             "taxon_concepts/", tax$id, 
                              "/distributions.xml", sep = ""),
                  httpheader = paste("X-Authentication-Token: ",
                                     cnx[[2]], sep = ""))
-  
-  temp <- getURI(url = paste(cnx[[1]], 
-                             "taxon_concepts.xml?=", query2, 
-                             "/distributions.xml", sep = ""),
-                 httpheader = paste("X-Authentication-Token: ",
-                                    cnx[[2]], sep = ""))
-  
-  
   temp2 <- xmlToList(temp)
+  if (country_only) {
+    data.frame(id = tax$id,
+               taxon = query_taxon,
+               distribution = unlist(sapply(temp2, '[', 'name')),
+               iso2 = unlist(sapply(temp2, '[', 'iso-code2')))
+  } else {
+    temp2
+  }
 }
 
 
