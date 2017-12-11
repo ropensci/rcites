db6c4d13043b626ba609599d39917c0fc6350701
Yasmine-Verzelen <yasmine.verzelen@inbo.be>
2017-12-11 16:42:34 +0100

    taxon_map

--- R/taxon_map.R
+++ R/taxon_map.R
@@ -1,5 +1,7 @@
 
 
+library(rworldmap)
+
 taxon_map <- function(cnx, taxon){
   map1 <- taxon_distribution(cnx, query_taxon = taxon, country_only = TRUE)
 map2 <- joinCountryData2Map(map1, joinCode="ISO2", nameJoinColumn = "iso2", nameCountryColumn = "distribution")
