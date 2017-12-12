36a55fa39702f4f3ab95118032a3076990cf6123
JonasGeschke <jonas.geschke@hotmail.de>
2017-12-12 16:36:45 +0100

    delete flattenlist function

--- R/flattenlist.R
+++
@@ -1,9 +0,0 @@
-flattenlist <- function(x){  
-  morelists <- sapply(x, function(xprime) class(xprime)[1]=="list")
-  out <- c(x[!morelists], unlist(x[morelists], recursive=FALSE))
-  if(sum(morelists)){ 
-    Recall(out)
-  }else{
-    return(out)
-  }
-}
