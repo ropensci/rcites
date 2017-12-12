b045555aad5c37b2ef3e0289b92286a38d89b836
JonasGeschke <jonas.geschke@hotmail.de>
2017-12-12 23:21:56 +0100

    cites_legistation work in progress

--- R/taxon_cites_legislation.R
+++ R/taxon_cites_legislation.R
@@ -16,16 +16,16 @@
 #' @export
 #' @examples
 #' # cnx <- sppplus_connect(token = 'ErJcYxUsIApHLCLOxiJ1Zwtt')
-#' # taxon_cites_legislation(cnx, tax_id = '4521', type = 'listing OR quota OR suspension')
+#' # taxon_cites_legislation(cnx, tax_id = '4521', type = 'listing')
 
 taxon_cites_legislation <- function(cnx, query_taxon = "Loxodonta africana", tax_id = NULL, 
-    citesleg_only = TRUE) {
+    type = "listing") {
     if (is.null(tax_id)) {
         tax <- sppplus_taxonconcept(cnx, query = query_taxon, appendix_only = TRUE)
     } else {
         tax <- data.frame(id = tax_id)
     }
-    temp <- getURI(url = paste(cnx[[1]], "taxon_concepts/", tax$id, "/cites_legislation.xml", 
+    temp <- getURI(url = paste(cnx[[1]], "taxon_concepts/", tax$tax_id, "/cites_legislation.xml", 
         sep = ""), httpheader = paste("X-Authentication-Token: ", cnx[[2]], sep = ""))
     
     temp <- xmlParse(temp)
@@ -36,10 +36,23 @@ taxon_cites_legislation <- function(cnx, query_taxon = "Loxodonta africana", tax
 #    listings <- temp2[[1]]["cites-listing"]
 #    quotas <- temp2[[2]]["cites-quota"]
 #    suspensions <- temp2[[3]]["cites-suspension"]
-    
-    listings <- xmlToDataFrame(unlist(temp2[[1]]["cites-listing"]))
-    quotas <- xmlToDataFrame(unlist(temp2[[2]]["cites-quota"]))
-    suspensions <- xmlToDataFrame(unlist(temp2[[3]]["cites-suspension"]))
+
+    if (type == "listing") {
+      listing <- xmlToDataFrame(unlist(temp2[[1]]["cites-listing"]))
+      listing
+    } else {
+      if (type == "quota") {
+        quota <- xmlToDataFrame(unlist(temp2[[2]]["cites-quota"]))
+        quota
+      } else {
+        if (type == "suspension") {
+          suspension <- xmlToDataFrame(unlist(temp2[[3]]["cites-suspension"]))
+          suspension
+        } else {
+          message("select type of legislation: listing, quota or suspension")
+        }
+      }
+    }
     
 #    length(names(listings))
 #    names(listings[[3]])
@@ -50,17 +63,16 @@ taxon_cites_legislation <- function(cnx, query_taxon = "Loxodonta africana", tax
 #    as.character(temp3[4]) # appendix
 #    as.character(temp3[6]) # date
 #    as.character(temp3[7]) # notes
-    
-    
 
-    if (type = "listing") {
-        data.frame(id = tax$id,
-                   taxon = ifelse(exists("query_taxon"), query_taxon, NA),
-                   country_iso2 = unlist(lapply(listings, "[", "party")),
-                   appendix = unlist(lapply(listings, "[", "appendix")),
-                   listing_date = unlist(lapply(listings, "[", "effective-at")),
-                   listing_notes = unlist(lapply(listings, "[", "annotation")))
-    } else {
-        message("select type of legislation: listing, quota or suspension")
-    }
+#    if (type = "listing") {
+#        data.frame(id = tax$id,
+#                   taxon = ifelse(exists("query_taxon"), query_taxon, NA),
+#                   country_iso2 = unlist(lapply(listings, "[", "party")),
+#                   appendix = unlist(lapply(listings, "[", "appendix")),
+#                   listing_date = unlist(lapply(listings, "[", "effective-at")),
+#                   listing_notes = unlist(lapply(listings, "[", "annotation")))
+#    } else {
+#        message("select type of legislation: listing, quota or suspension")
+#    }
+
 }
