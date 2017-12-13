c18189892c69e289cc3b1c74164a4c334ad67cc8
JonasGeschke <jonas.geschke@hotmail.de>
2017-12-13 14:03:47 +0100

    query_taxon

--- R/sppplus_taxonconcept.R
+++ R/sppplus_taxonconcept.R
@@ -2,43 +2,51 @@
 #'
 #' Queries CITES species+ API using connection generated from \code{\link[citesr]{sppplus_connect}}. The query string filters species+ data by taxon concept (e.g.species, genus, class)
 #'
-#' @param cnx species+ connection information (see \code{\link[citesr]{sppplus_connect}})
-#' @param query character string containing the query (e.g. species)
-#' @param appendix_only a logical statement for querying only the appendix number information. Default is TRUE.
+#' @param cnx species+ connection information (see \code{\link[citesr]{sppplus_connect}}).
+#' @param query_taxon character string containing the query (e.g. species).
+#' @param appendix_only logical statement for querying only the taxon id and CITES appendix information. Default is TRUE.
 #' @return If appendix_only is TRUE, returns a dataframe with taxon and cites_listing. If appendix_only is FALSE, returns a list with all taxon_concept information.
 #'
 #' @importFrom RCurl getURI
-#' @importFrom XML xmlToList xmlRoot xmlParse xmlToDataFrame xmlName
+#' @importFrom XML xmlToList
 #'
 #' @export
 #' @examples
 #' # cnx <- sppplus_connect(token = 'insert your token here')
-#' # sppplus_taxonconcept(cnx, query = 'Loxodonta africana', appendix_only = TRUE)
-#' # sppplus_taxonconcept(cnx, query = 'Homo sapiens', appendix_only = TRUE)
+#' # sppplus_taxonconcept(cnx, query_taxon = 'Loxodonta africana', appendix_only = TRUE)
+#' # sppplus_taxonconcept(cnx, query_taxon = 'Homo sapiens', appendix_only = TRUE)
 
-sppplus_taxonconcept <- function(cnx, query = "Loxodonta africana", appendix_only = TRUE) {
+sppplus_taxonconcept <- function(cnx, query_taxon = "Loxodonta africana", appendix_only = TRUE) {
     # we can add here a check t ensure is a valid name
-    query2 <- gsub(pattern = " ", replacement = "%20", x = query)
-    temp <- getURI(url = paste(cnx[[1L]], "taxon_concepts.xml?name=", query2, sep = ""),
+    query <- gsub(pattern = " ", replacement = "%20", x = query_taxon)
+    temp <- getURI(url = paste(cnx[[1L]], "taxon_concepts.xml?name=", query, sep = ""), 
         httpheader = paste("X-Authentication-Token: ", cnx[[2L]], sep = ""))
-    temp2 <- xmlRoot(xmlParse(temp))
-    if (xmlToList(unlist(temp2[[1L]][[3L]]))$text == "0") {
-        message("species not listed in CITES")
+    temp2 <- xmlParse(temp)
+    temp2 <- xmlRoot(temp2)
+    if (xmlToList(unlist(temp2[[1]][[3]]))$text == "0") {
+      message("species not listed in CITES")
     } else {
-        if (appendix_only) {
-            temp3 <- xmlToDataFrame(unlist(temp2[[2L]]["taxon-concept"]))
-            temp3 <- temp3[c(1, 2, 8)]
-            names(temp3) <- c("tax_id", "species", "appendix")
-            temp3
-        } else {
-            xmlToList(temp)
-        }
+      if (appendix_only == TRUE) {
+        temp3 <- xmlToDataFrame(unlist(temp2[[2]]["taxon-concept"]))
+        temp3 <- temp3[c(1,2,8)]
+        names(temp3) <- c("tax_id", "species", "appendix")
+        temp3
+    } else {
+        xmlToList(temp)
+    }
     }
 
-    # temp2 <- xmlToList(temp) if (temp2$pagination$`total-entries`$text == '0') {
-    # message('species not listed in CITES') } else { if (appendix_only) {
-    # data.frame(id = temp2$`taxon-concepts`$`taxon-concept`$id$text, species =
-    # temp2$`taxon-concepts`$`taxon-concept`$`full-name`, appendix =
-    # temp2$`taxon-concepts`$`taxon-concept`$`cites-listing`) } else { temp2 } }
+#    temp2 <- xmlToList(temp)
+#    if (temp2$pagination$`total-entries`$text == "0") {
+#      message("species not listed in CITES")
+#    } else {
+#      if (appendix_only) {
+#        data.frame(id = temp2$`taxon-concepts`$`taxon-concept`$id$text, 
+#                   species = temp2$`taxon-concepts`$`taxon-concept`$`full-name`,
+#                   appendix = temp2$`taxon-concepts`$`taxon-concept`$`cites-listing`)
+#      } else {
+#        temp2
+#      }
+#    }
 
 }
