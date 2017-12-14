b9b92110d2e9b475bcc80eba18bf73680e25c579
JonasGeschke <jonas.geschke@hotmail.de>
2017-12-14 16:49:16 +0100

    rename distribution map ### still to be written!

--- R/distribution_map.R
+++ R/distribution_map.R
@@ -1,8 +1,8 @@
-taxon_map <- function(cnx, taxon) {
+distribution_map <- function(cnx, taxon) {
     map1 <- taxon_distribution(cnx, tax_id = taxon, country_only = TRUE)
     map2 <- rworldmap::joinCountryData2Map(map1, joinCode = "ISO2", nameJoinColumn = "iso2",
         nameCountryColumn = "distribution")
     rworldmap::mapCountryData(mapToPlot = map2, nameColumnToPlot = "taxon", mapTitle = taxon,
         addLegend = FALSE)
 }
-# taxon_map(cnx, 'Branta canadensis')
+# distribution_map(cnx, 'Branta canadensis')
