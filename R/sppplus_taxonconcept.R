7540050a45a62a3f85edc9448fa601736a7c7880
Kevin Cazelles <kcazelle@uoguelph.ca>
2018-03-09 12:12:24 -0500

    declare := operator

--- R/sppplus_taxonconcept.R
+++ R/sppplus_taxonconcept.R
@@ -25,14 +25,14 @@
 
 sppplus_taxonconcept <- function(query_taxon, token = NULL, appendix_only = TRUE) {
     # token check
-    if (is.null(token))
+    if (is.null(token)) 
         token = sppplus_getsecret()
     # 2Bdone: add here a check to ensure is a valid name
     query <- gsub(pattern = " ", replacement = "%20", x = query_taxon)
-    #
+    # 
     q_url <- sppplus_url(paste0("taxon_concepts.json", "?name=", query))
     res <- sppplus_res(q_url, token)
-
+    
     if (!res$pagination$total_entries) {
         warning("taxon not listed in CITES")
         out <- NULL
