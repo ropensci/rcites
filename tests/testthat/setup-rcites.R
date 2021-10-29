9b71ab3534bc9aac3a45b77599b37e267ed29989
Kevin Cazelles <kevcaz@protonmail.com>
2021-10-29 14:45:56 -0400

    introduce vcr see #56

---
+++ tests/testthat/setup-rcites.R
@@ -0,0 +1,24 @@
+library("vcr") # *Required* as vcr is set up on loading
+
+# see https://books.ropensci.org/http-testing/vcr.html
+
+vcr_dir <- vcr::vcr_test_path("fixtures") 
+
+if (!nzchar(Sys.getenv("SPECIESPLUS_TOKEN"))) {
+  if (dir.exists(vcr_dir)) {
+    # Fake API token to fool our package
+    Sys.setenv("SPECIESPLUS_TOKEN" = "hackme")
+  } else {
+    # If there's no mock files nor API token, impossible to run tests
+    stop("No API key nor cassettes, tests cannot be run.",
+         call. = FALSE)
+  }
+}
+
+
+invisible(vcr::vcr_configure(
+  dir = vcr_dir,
+  filter_request_headers = list('X-Authentication-Token' = "safe")
+))
+
+vcr::check_cassette_names()
--- tests/testthat/test-distributions.R
+++ tests/testthat/test-distributions.R
@@ -1,57 +1,63 @@
 context("Distributions")
 
-skip_on_cran()
-skip_if_no_auth()
-ut_pause()
-
-res1 <- spp_distributions(taxon_id = tx_id)
-res1b <- spp_distributions(taxon_id = tx_id2, verbose = FALSE)
-res2 <- spp_distributions(taxon_id = tx_id, raw = TRUE, verbose = FALSE)
-res3 <- spp_distributions(taxon_id = tx_id, language = "fr", verbose = FALSE)
-res4 <- spp_distributions(taxon_id = tx_id, language = "es", verbose = FALSE)
-
 lang_GQ <- c("Equatorial Guinea", "Guinée équatoriale", "Guinea Ecuatorial")
-lang_en <- lang_GQ %in% res1$distributions$name
-lang_fr <- lang_GQ %in% res3$distributions$name
-lang_es <- lang_GQ %in% res4$distributions$name
 
-test_that("Expected classes", {
-  expect_equal(class(res1), "spp_distr")
-  expect_equal(class(res1[1L]), "list")
-  expect_true(all(unlist(lapply(res1, function(x) all(class(x) == cl_df)))))
-  expect_true(all(class(res2) == cl_raw))
-  vc_ch <- c("id", "iso_code2", "name", "type", "tags")
-  expect_true(all(names(res1[[1L]]) == vc_ch))
-  expect_true(all(names(res1[[2L]]) == c("id", "reference")))
-  expect_null(print(res1))
-  expect_null(print(res2))
+
+test_that("spp_distributions() default works", {
+  vcr::use_cassette("spp_distributions_def", {
+    res <- spp_distributions(taxon_id = tx_id)
+  })  
+  expect_equal(class(res), "spp_distr")
+  expect_equal(class(res[1L]), "list")
+  expect_true(all(unlist(lapply(res, function(x) all(class(x) == cl_df)))))
+  expect_identical(lang_GQ %in% res$distributions$name, c(TRUE, FALSE, FALSE))
+  # expect_null(print(res))
 })
 
-test_that("Language", {
-  expect_true(all(lang_en == c(TRUE, FALSE, FALSE)))
-  expect_true(all(lang_fr == c(FALSE, TRUE, FALSE)))
-  expect_true(all(lang_es == c(FALSE, FALSE, TRUE)))
+test_that("spp_distributions() verbose mode works", {
+  vcr::use_cassette("spp_distributions_ver", {
+    expect_silent(res <- spp_distributions(taxon_id = tx_id, verbose = FALSE))
+  })  
+})
+
+test_that("spp_distributions() raw mode works", {
+  vcr::use_cassette("spp_distributions_raw", {
+    res <- spp_distributions(taxon_id = tx_id, raw = TRUE, verbose = FALSE)
+  })  
+  expect_true(all(class(res) == cl_raw))
+  # expect_null(print(res))
 })
 
 
-res5 <- spp_distributions(taxon_id = c(tx_id, tx_id2), verbose = FALSE)
-res6 <- spp_distributions(taxon_id = c(tx_id, tx_id2), raw = TRUE,
-  verbose = FALSE)
-test_that("distr_multi outputs", {
-  expect_equal(class(res5), "spp_distr_multi")
-  expect_equal(class(res5$distributions), cl_df)
-  expect_equal(nrow(res5$distributions),
-    nrow(res1$distributions) + nrow(res1b$distributions))
-  expect_identical(unique(res5$distributions$taxon_id), c(tx_id, tx_id2))
-  expect_identical(class(res6), cl_raw_multi)
-  expect_equal(class(res6[[1L]]), cl_raw)
-  expect_equal(length(res6), 3)
-  expect_identical(res6$taxon_id, c(tx_id, tx_id2))
+test_that("spp_distributions() language works", {
+  vcr::use_cassette("spp_distributions_lan", {
+    res1 <- spp_distributions(taxon_id = tx_id, language = "fr", 
+      verbose = FALSE)
+    res2 <- spp_distributions(taxon_id = tx_id, language = "es", 
+      verbose = FALSE)
+  })  
+  expect_identical(lang_GQ %in% res1$distributions$name, c(FALSE, TRUE, FALSE))
+  expect_identical(lang_GQ %in% res2$distributions$name, c(FALSE, FALSE, TRUE))
 })
-ut_pause()
+
+test_that("spp_distributions() batch mode works", {
+  vcr::use_cassette("spp_distributions_bat", {
+    res1 <- spp_distributions(taxon_id = c(tx_id, tx_id2), verbose = FALSE)
+    res2 <- spp_distributions(taxon_id = c(tx_id, tx_id2), raw = TRUE,
+      verbose = FALSE)
+  })  
+  expect_identical(unique(res1$distributions$taxon_id), c(tx_id, tx_id2))
+  expect_equal(class(res1), "spp_distr_multi")
+  expect_identical(class(res2), cl_raw_multi)
+  expect_equal(length(res2), 3)
+  expect_identical(res2$taxon_id, c(tx_id, tx_id2))
+})
 
 # taxon with only one distribution
-res7 <- spp_distributions("10000", verbose = FALSE)
-test_that("only one distribution", {
-  expect_equal(dim(res7$distribution)[1], 1)
+test_that("spp_distributions() edge case: one distribution", {
+  vcr::use_cassette("spp_distributions_one", {
+    res <- spp_distributions("10000", verbose = FALSE)
+  })  
+  expect_equal(dim(res$distribution)[1], 1)
 })
+
